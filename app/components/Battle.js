7c25b651c9d5e2126e0df9660aabb1c845499e29
Renee Leung <rleung@pagerduty.com>
2019-03-06 14:23:37 -0500

    forms and controlled components, encapsulation - battle input field

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,96 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            username: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this); /* allows 'this' in handleChange to refer to 'this' component */
+        this.handleSubmit = this.handleSubmit.bind(this); /* allows 'this' in handleSubmit to refer to 'this' component */
+    }
+
+    handleChange(event) {
+        var value = event.target.value;
+        
+        this.setState(function() {
+            return {
+                username: value
+            }
+        })
+    }
+    handleSubmit(event) {
+        event.preventDefault(); /* avoids form to submit to a server */
+        this.props.onSubmit(this.props.id, this.state.username); /* call parent's handleSubmit */
+    }
+
+    render() {
+        return (
+            <form className='column' onSubmit={this.handleSubmit /* button is 'submit' type */}>
+                <label className='header' htmlFor='username'>
+                    {this.props.label}
+                </label>
+                <input
+                    id='username'
+                    placeholder='github username'
+                    type='text'
+                    autoComplete='off' 
+                    alue={this.state.username}
+                    onChange={this.handleChange} 
+                />
+                <button
+                    className='button'
+                    type='submit'
+                    disabled={!this.state.username}>
+                    Submit
+                </button>
+            </form>
+        )
+    }
+}
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
+}
 
 class Battle extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerOneName: '',
+            playerTwoName: '',
+            playerOneImage: null,
+            playerTwoImage: null
+        }
+        this.handleSubmit = this.handleSubmit.bind(this); /* Whenever handleSubmit is called, it will bind to 'this' component */
+    }
+
+    handleSubmit(id, username) {
+        this.setState(function() {
+            var newState = {};
+            newState[id + 'Name'] = username;
+            newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+            return newState;
+        });
+    }
     render () {
+        var playerOneName = this.state.playerOneName;
+        var playerTwoName = this.state.playerTwoName;
         return (
             <div>
-                BATTLE!
+                <div className='row'>
+                    {!playerOneName &&
+                        <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit}/>
+                    }
+                    {!playerTwoName &&
+                        <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit}/>
+                    }
+                </div>
             </div>
         )
     }
